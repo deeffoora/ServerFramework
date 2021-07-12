2333b04bc7dd3bbea4d65f9d0649c7f8c3a097de
Sergey <deeffoora@gmail.com>
2021-07-12 18:08:28 +0300

    local repositories synchronization
    local repositories synchronization and a test commit

--- ServerFramework_01/Program.cs
+++ ServerFramework_01/Program.cs
@@ -4,7 +4,7 @@ namespace ServerFramework_01
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("Hello World!");
         }
