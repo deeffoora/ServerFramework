124dde453ede398d2f9f9eb0a51e44ad085724e7
Иванов Сергей Васильевич <deeffoora@gmail.com>
2021-07-31 16:10:14 +0300

    Update Program.cs
    Create udp socket

--- ServerFramework_01/Program.cs
+++ ServerFramework_01/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
 
 namespace ServerFramework_01
 {
     class Program
     {
+        private static UdpClient _socket = new UdpClient();
+
         static void Main()
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                IPEndPoint ep = null;
+                Console.WriteLine(_socket.Client.LocalEndPoint.ToString());
+                _ = _socket.Receive(ref ep);
+            }
         }
     }
 }
